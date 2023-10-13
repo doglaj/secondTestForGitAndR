2a46a653b3db5522a2fc1d006fae643048bf443e
Julien REMONDEAU (ACER) <Julien.REMONDEAU@acer.europa.eu>
2023-10-13 13:00:27 +0200

    commit plot

--- randomRscript.R
+++ randomRscript.R
@@ -1,3 +1,15 @@
 library(tidyverse)
 
 #here a comment
+
+library(ggplot2)
+
+head(cars)
+plot <- cars %>% ggplot() + aes(speed, dist) + geom_point() + labs(
+  title = "Stopping Distances of Cars",
+  x = "Speed (mph)",
+  y = "Stopping Distance (ft)"
+)
+
+ggsave('plot.png',plot=plot)
+#new change I want to revert
